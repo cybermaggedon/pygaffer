14a678f906e31f84049ccaa161c04d981e053fe4
Mark Adams <cybermaggedon@gmail.com>
2019-04-16 10:02:45 -0700

    Modified setup to work with pypi, ported get-30 to Python 3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="gaffer",
+    name="pygaffer",
     version="0.1",
     author="cybermaggedon",
     author_email="cybermaggedon@gmail.com",
@@ -13,6 +13,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/cybermaggedon/pygaffer",
     packages=setuptools.find_packages(),
+    include_package_data=True,
     install_requires=[
         'requests'
     ],
